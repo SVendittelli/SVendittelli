fc88e818f780f815763cfe6da22bcb2e51b7bb29
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-12-27 16:50:32 +0000

    Update hire-me.sh

--- hire-me.sh
+++ hire-me.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "I'm looking for a new job, if you're interested in hiring me, please find my contact details below"
+echo "I'm not looking for a new job right now, but if you're interested in hiring me, please find my contact details below"
 
 # vim:ff=unix
