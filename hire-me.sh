2b66e17f3455933783ebe21664b98a5ed487a544
Sam Vendittelli <sam.vendittelli@hotmail.com>
2024-12-27 16:55:38 +0000

    Update hire-me.sh
    Break message over multiple lines

--- hire-me.sh
+++ hire-me.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo "I'm not looking for a new job right now, but if you're interested in hiring me, please find my contact details below"
+echo "I'm not looking for a new job right now, but if"
+echo "you're interested in hiring me, please find my"
+echo "contact details below"
 
 # vim:ff=unix
